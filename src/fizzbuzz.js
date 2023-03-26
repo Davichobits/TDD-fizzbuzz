0b59863c51bb78c8bb79ccff8a6964927250093c
Davichobits <davidruizdeveloper@gmail.com>
2023-03-26 05:25:53 -0500

    refactorizacion de funcion

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,8 +2,18 @@
 export const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('parameter provided must be a number')
   if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
-  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
-  if (number % 3 === 0) return 'fizz'
-  if (number % 5 === 0) return 'buzz'
-  return number
+  // if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
+  // if (number % 3 === 0) return 'fizz'
+  // if (number % 5 === 0) return 'buzz'
+  // return number
+
+  const multiples = { 3: 'fizz', 5: 'buzz' }
+  let output = ''
+
+  Object
+    .entries(multiples)
+    .forEach(([multiple, word]) => {
+      if (number % multiple === 0) output += word
+    })
+  return output === '' ? number : output
 }
