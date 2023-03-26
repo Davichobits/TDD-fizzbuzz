7afd26d99335d4059aa3ce040bbb30d389ea9028
Davichobits <davidruizdeveloper@gmail.com>
2023-03-26 04:42:54 -0500

    funcion separada en otro archivo

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,13 +1,5 @@
 import { describe, expect, it } from 'vitest'
-
-const fizzbuzz = (number) => {
-  if (typeof number !== 'number') throw new Error('parameter provided must be a number')
-  if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
-  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
-  if (number % 3 === 0) return 'fizz'
-  if (number % 5 === 0) return 'buzz'
-  return number
-}
+import { fizzbuzz } from '../src/fizzbuzz'
 
 describe('fizzbuzz', () => {
   it('should be a function', () => {
